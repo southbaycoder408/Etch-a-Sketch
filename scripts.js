4ae1d8577143343ced49e987fd38d8c1b0aa9702
southbaycoder408 <jedheadgaming@gmail.com>
2022-02-22 20:16:09 -0800

    added function that creates 256 divs which will need to be turned in 16x16 grid

--- scripts.js
+++ scripts.js
@@ -5,8 +5,19 @@ document.body.appendChild(container);
 //add class "container" to container div
 container.classList.add('container');
 
+function createSquare() 
+{
+    let i = 0;
+    for(i; i<256; i++){
+    //creates div which will be the squares in container
+    const square = document.createElement('div');
+    //adds square class to square div
+    square.classList.add('square');
+    square.style.border = "1px solid black";
+    square.style.padding = "5px 5px 5px";
+    //adds square div to container 
+    container.appendChild(square);
+    }
+}
 
-//creates div which will be the squares in container
-const square = document.createElement('div');
-//adds to 
-container.appendChild(square);
+createSquare();
