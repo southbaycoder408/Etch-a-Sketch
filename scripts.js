06e4f1d156d6babd4ab19cd275182b2fc176a946
southbaycoder408 <jedheadgaming@gmail.com>
2022-02-26 01:00:51 -0800

    edited code to update canvas to requested prompt size and also readds event listener after initial parse of code

--- scripts.js
+++ scripts.js
@@ -2,12 +2,17 @@ const button = document.createElement('button');
 button.textContent = "RESET";
 document.body.appendChild(button);
 
-button.addEventListener('click', ()=> {
-    allSquare.forEach((square) =>{
+let squarePerSide;
+
+button.addEventListener('click', () => {
+    allSquare.forEach((square) => {
         square.style.backgroundColor = "";
-        
+
     })
-    prompt("Please input the the # of squares per side for new canvas", "16 squares";
+    squarePerSide = prompt("Please input the the # of squares per side for new canvas");
+    Number(squarePerSide);
+    console.log(squarePerSide);
+    createSquare();
 });
 
 //creates div container that will hold square div 16x16 grid 
@@ -20,34 +25,71 @@ container.classList.add('container');
 //function that creates 16 "row" elements and 16"square" elements inside that...
 //designed this way to make use of flexbox and organization of square
 function createSquare() {
-    let row = 1;
-    for (row; row <= 16; row++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        container.appendChild(row);
 
-        let box = 1;
+    let row = 0;
+    Number(squarePerSide);
 
-        for (box; box <= 16; box++) {
-            //creates div which will be the squares in container
-            const square = document.createElement('div');
-            //adds square class to square div
-            square.classList.add('square');
+    if (squarePerSide === undefined) { //condition guarentees the default canvas size is 16x16 square
+        for (row; row <= 15; row++) {
+            const row = document.createElement('div');
+            row.classList.add('row');
+            container.appendChild(row);
 
+            let box = 1;
 
-            //square.textContent = "Square #" + box;
+            for (box; box <= 16; box++) {
+                //creates div which will be the squares in container
+                const square = document.createElement('div');
+                //adds square class to square div
+                square.classList.add('square');
 
-            //adds square div to container 
-            row.appendChild(square);
+                //adds square div to container 
+                row.appendChild(square);
+            }
         }
+    } else if (squarePerSide == 0) {
+        alert("Please choose a number greater than 0!");
+    } else {
+        const allRow = document.getElementsByClassName('row');
+        while (allRow[0]) {
+            allRow[0].parentNode.removeChild(allRow[0]);
+        }
+        for (row; row <= (squarePerSide - 1); row++) {
+            const row = document.createElement('div');
+            row.classList.add('row');
+            container.appendChild(row);
+
+            let box = 1;
+
+            for (box; box <= (squarePerSide); box++) {
+                //creates div which will be the squares in container
+                const square = document.createElement('div');
+                //adds square class to square div
+                square.classList.add('square');
+
+
+                //square.textContent = "Square #" + box;
+
+                //adds square div to container 
+                row.appendChild(square);
+            }
+        }
+        //after conditions are met and squares are made, this is same as below and sets up event listener for squares
+        const allSquare = document.querySelectorAll('.square');
+        allSquare.forEach((square) => {
+            square.addEventListener('mouseover', (event) => {
+                event.target.style.backgroundColor = "blue";
+            })
+        });
     }
+
 }
 
 createSquare();
 
 //creates variable that stores nodelist of "square" classes
 const allSquare = document.querySelectorAll('.square');
-
+//colors squares with event listener
 allSquare.forEach((square) => {
     square.addEventListener('mouseover', (event) => {
         //highlights the text of each class square to target color
